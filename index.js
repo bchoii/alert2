01c5a516ec7639cd20ad84e2baadb04f37b25767
bchoii <bchoii@gmail.com>
2018-02-17 23:39:37 +0800

    .

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ module.exports = function(content) {
     const message = document.createElement("div");
     message.style.cssText =
       "display:inline-block;padding:10vh;background:white;border:1px solid #ccc;";
-    message.classList.add("fadein");
     message.textContent = content;
 
     const backdrop = document.createElement("div");
     backdrop.style.cssText =
       "background:rgba(255, 255, 255, 0.8);position:absolute;top:0;left:0;bottom:0;right:0;text-align:center;padding:10vh;";
+    backdrop.classList.add("alert");
+    backdrop.classList.add("fadein");
     backdrop.appendChild(message);
     backdrop.onclick = async e => {
-      await animate(message, "fadeout");
+      await animate(backdrop, "fadeout");
       document.body.removeChild(backdrop);
       res();
     };
