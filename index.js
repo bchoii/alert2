88a45cf645edc5d3c3785e1f8930d2b9e80a5ea4
bchoii <bchoii@gmail.com>
2018-03-17 15:38:14 +0800

    .

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var background;
+let alertbackground;
 
-function removeChildren(node) {
-  var cNode = node.cloneNode(false);
-  node.parentNode.replaceChild(cNode, node);
+function removeNode(node) {
+  node && node.parentNode && node.parentNode.removeChild(node);
 }
 
+// function removeChildren(node) {
+//   var cNode = node.cloneNode(false);
+//   node.parentNode.replaceChild(cNode, node);
+// }
+
 function animate(element, clazz) {
+  if (!element) return;
   return new Promise((res, rej) => {
     element.addEventListener("animationend", () => {
       res();
@@ -16,38 +21,30 @@ function animate(element, clazz) {
 
 function show(content) {
   return new Promise(async (res, rej) => {
-    console.log("DEBUG");
-    const button = document.createElement("div");
-    button.textContent = "Ok";
-    button.onclick = () => {
-      hide(res);
-    };
-    button.focus();
-
+    await hide();
     const messagebox = document.createElement("div");
     messagebox.classList.add("messagebox");
-    messagebox.textContent = content;
-    messagebox.appendChild(document.createElement("hr"));
-    messagebox.appendChild(button);
 
-    // hide();
-    background = document.createElement("div");
-    background.classList.add("alert2");
-    background.classList.add("background");
-    background.classList.add("fadein");
-    background.appendChild(messagebox);
-    document.body.appendChild(background);
+    alertbackground = document.createElement("div");
+    alertbackground.classList.add("alert2");
+    alertbackground.classList.add("background");
+    alertbackground.classList.add("fadein");
+    alertbackground.appendChild(messagebox);
+
+    alertbackground.onclick = () => {
+      hide();
+      res();
+    };
+
+    messagebox.textContent = content;
+    document.body.appendChild(alertbackground);
   });
 }
 
-async function hide(done) {
-  if (background) {
-    await animate(background, "fadeout");
-    document.body.removeChild(background);
-  }
-  if (done) {
-    done();
-  }
+async function hide() {
+  await animate(alertbackground, "fadeout");
+  removeNode(alertbackground);
+  alertbackground = undefined;
 }
 
 module.exports = { show, hide };
